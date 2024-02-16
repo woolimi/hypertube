6c622566670f72509f5419cfff11a843ef0791f8
Sungyong Cho <dev@sungyongcho.com>
2024-02-16 14:48:04 +0100

    feat: enabled swagger (#10)
    Swagger 모듈을 추가하여 api 문서를 바로 확인하실 수 있습니다.
    
    코드에도 나와있지만 main.ts 에서 { DocumentBuilder, SwaggerModule }을 불러 온 이후에
    DocumentBuilder 객체 생성 -> swaggermodule에서 document 생성, setup 진행하면 바로
    사용합니다.
    
    http://localhost:5000/api 로 연결하시면 바로 확인가능하고 추후에 경로 수정 가능합니다. 
    
    swagger 확인용으로 UserDTO 클래스도 만들어 놓았습니다.
    
    @ApiOperation, @ApiOkResponse 데코레이터를 잘 설정 해놓으면 swagger위에서도 바로 req/res
    테스트를 해 볼수 있으니 한번 확인 해보시는걸 추천드립니다.
    그리고 swagger 가 생성해 놓은것들 두루두루 보시면 좋을것 같네요
    
    
    ![image](https://github.com/woolimi/hypertube/assets/46742040/acc7a9cf-dd39-4009-8b61-39ba5bbfabeb)

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,13 @@ async function bootstrap() {
       credentials: true,
     },
   });
+  const config = new DocumentBuilder()
+    .setTitle('Hypertube API')
+    .setDescription('API Documentation for Hypertube.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
---
+++ back/src/user/dto/user.dto.ts
@@ -0,0 +1,3 @@
+import { User } from 'src/user/user.entity';
+
+export class UserDto extends User { }
