6b5c681b4086f969a7c2d033deb54ebdb79d1a6c
kyubong <kchoi@nomination.fr>
2024-02-19 22:36:17 +0100

    gitignore update

---
+++ back/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as fs from 'fs';
+
+@Controller('')
+export class AppController {
+  @Get('/images/avatar/:path')
+  async getAvatar(@Param('path') path: string, @Res() res: Response) {
+    const imagePath = 'images/avatar/' + path;
+
+    if (fs.existsSync(imagePath)) res.sendFile(imagePath, { root: '.' });
+    // else res.sendFile('images/no_image.png', { root: '.' });
+  }
+}
---
+++ back/src/user/dto/update-user.dto.ts
@@ -0,0 +1,47 @@
+import {
+  IsNotEmpty,
+  IsEmail,
+  Length,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @Length(2, 20)
+  @IsOptional()
+  firstName?: string;
+
+  @IsNotEmpty()
+  @Length(2, 20)
+  @IsOptional()
+  lastName?: string;
+
+  @IsNotEmpty()
+  @Length(2, 20)
+  @IsOptional()
+  username?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(40)
+  @IsOptional()
+  email?: string;
+
+  @IsNotEmpty()
+  @Length(8, 20)
+  @IsOptional()
+  password?: string;
+
+  @IsOptional()
+  @IsOptional()
+  image?: string;
+
+  @IsOptional()
+  @IsOptional()
+  provider?: string;
+
+  @IsOptional()
+  @IsOptional()
+  emailVerified?: boolean;
+}
