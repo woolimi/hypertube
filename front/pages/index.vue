46a51c2e15f39a01316e07993f6461d516769e1e
Woolim Park <woolimi91@naver.com>
2024-02-22 20:06:46 +0100

    infinite scroll

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { useWindowScroll } from "@vueuse/core";
+
+const { y } = useWindowScroll();
 const axios = useAxios();
 const info = reactive({
   page: 1,
@@ -8,19 +11,45 @@ const info = reactive({
 
 const { localeProperties } = useI18n();
 const movies = ref([]);
+const fetching = ref(false);
 
-onMounted(async () => {
-  const { data } = await axios.get("/movies/", {
+const fetchMovies = async (page, language) => {
+  return await axios.get("/movies/", {
     params: {
-      page: info.page,
-      language: localeProperties.value.iso,
+      page,
+      language,
     },
   });
+};
+onMounted(async () => {
+  fetching.value = true;
+  const { data } = await fetchMovies(info.page, localeProperties.value.iso);
   const { page, total_pages, total_results, results } = data;
   info.page = page;
   info.total_pages = total_pages;
   info.total_results = total_results;
   movies.value = results;
+  fetching.value = false;
+});
+
+watch(y, async (scrolledHeight) => {
+  if (fetching.value) return;
+  if (info.page >= info.total_pages) return;
+
+  const totalScrollHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
+  if (totalScrollHeight - scrolledHeight <= 200) {
+    info.page += 1;
+
+    fetching.value = true;
+    const { data } = await fetchMovies(info.page, localeProperties.value.iso);
+    const { page, total_pages, total_results, results } = data;
+    info.page = page;
+    info.total_pages = total_pages;
+    info.total_results = total_results;
+    movies.value = [...movies.value, ...results];
+    fetching.value = false;
+  }
 });
 </script>
 
