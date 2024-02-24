f102be2db6e26fff63cb95e87cb3ad181eeeae8f
Sungyong Cho <dev@sungyongcho.com>
2024-02-24 16:10:09 +0100

    feat: password email change complete page created

---
+++ front/pages/auth/password-email-complete.vue
@@ -0,0 +1,22 @@
+<script setup>
+definePageMeta({
+  layout: "password-change",
+  middleware: ["non-auth"],
+});
+</script>
+
+<template>
+  <div class="mt-10 flex w-full max-w-[450px] flex-col items-center gap-4">
+    <h1 class="text-center text-3xl font-[800] sm:text-4xl lg:mb-10">
+      {{ $t("AuthForgotPassword.complete.title") }}
+    </h1>
+    <p class="text-center text-2xl text-gray-400">
+      {{ $t("AuthForgotPassword.complete.description") }}
+    </p>
+    <div>
+      <NuxtLink :to="localePath('/auth/login')">
+        <Button :label="$t('AuthForgotPassword.complete.button')" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
