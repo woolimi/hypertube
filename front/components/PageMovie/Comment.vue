a64f730768a3860045774106a861db263aa8d14c
jungmoo cheon <developer.cheon@gmail.com>
2024-02-29 17:07:09 +0100

    feat: (FRONT) add validation of editing only the user's comments

--- front/components/PageMovie/Comment.vue
+++ front/components/PageMovie/Comment.vue
@@ -13,6 +13,7 @@ const props = defineProps({
   },
 });
 
+const { userData } = storeToRefs(useUserStore());
 const axios = useAxios();
 const editState = ref(false);
 
@@ -20,7 +21,7 @@ const editComment = () => {
   // console.log('is editing:', props.isEditing, 'edit state:', editState.value)
   if (props.isEditing) {
     console.log(props.isEditing, "is editing");
-	// TODO: update alert
+    // TODO: update alert
     alert("You will lose any unsaved changes to your message.");
     return;
   }
@@ -41,11 +42,14 @@ const deleteComment = async () => {
     console.error(error);
   }
 };
+// console.log('userData:', userData.value?.username == props.item.User.username)
 </script>
 
 <template>
   <div class="flex gap-3">
-    <NuxtLink :to="localePath({ name: 'users-uid', params: { uid: 123 } })">
+    <NuxtLink
+      :to="localePath({ name: 'users-uid', params: { uid: userData.id } })"
+    >
       <Avatar
         :image="item.User.image"
         class="shrink-0 overflow-hidden"
@@ -60,8 +64,10 @@ const deleteComment = async () => {
       <p v-else>{{ item.content }}</p>
     </aside>
   </div>
-  <!-- TODO: edit/delete only my comments -->
-  <div v-if="!editState" class="text-right">
+  <div
+    v-if="!editState && userData.username == item.User.username"
+    class="text-right"
+  >
     <button
       class="rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-700"
       @click="editComment"
