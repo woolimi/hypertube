0bb80ca1e978b74d1b1a02f53f5578f78f67a8a0
Kyubong <64304902+jeanoza@users.noreply.github.com>
2024-03-06 21:56:51 +0100

    Kychoi/feature/file image size validation (#60)
    https://github.com/users/woolimi/projects/2?pane=issue&itemId=55467666
    
    ---------
    
    Co-authored-by: kyubong <kchoi@nomination.fr>

--- front/composables/useProfile.ts
+++ front/composables/useProfile.ts
@@ -2,18 +2,32 @@ import type { AxiosInstance } from "axios";
 
 import type { UserData } from "~/types";
 
+const MAX_IMAGE_SIZE = 5242880; // 5MB
+
+const imageAlertActive = ref(false);
+
 export const useProfile = () => {
   const axios = useAxios();
   const router = useRouter();
 
   function updateAvatar(event: any) {
-    const file = event.target.files[0];
+    const target = event.target;
+    const file = target.files[0];
     if (file) {
+      if (file.size > MAX_IMAGE_SIZE) {
+        imageAlertActive.value = true;
+        target.value = "";
+        return setTimeout(() => {
+          imageAlertActive.value = false;
+        }, 5000);
+      }
+
       const reader = new FileReader();
 
       reader.onload = async () => {
         const formData = new FormData();
         formData.append("image", file);
+
         try {
           await axios.patch(`/users/avatar`, formData);
           router.go();
@@ -34,6 +48,7 @@ export const useProfile = () => {
   };
 
   return {
+    imageAlertActive,
     updateAvatar,
     updateProfile,
   };
