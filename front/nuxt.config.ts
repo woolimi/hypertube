2d20b70f079e7c53a0064714127d6db806ce681c
jungmoo cheon <developer.cheon@gmail.com>
2024-03-07 03:45:23 +0100

    chore: optimize docker

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -61,6 +61,7 @@ export default defineNuxtConfig({
   },
   i18n: {
     vueI18n: "./i18n.config.ts",
+    defaultLocale: "en",
     detectBrowserLanguage: {
       useCookie: true,
       fallbackLocale: "en",
