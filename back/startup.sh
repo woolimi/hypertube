721367c2b43238553242dae1821c64cae512da90
Sungyong Cho <dev@sungyongcho.com>
2024-03-09 03:48:46 +0100

    fix: move dependencies around in order to make ts-node comnmand available

--- back/startup.sh
+++ back/startup.sh
@@ -3,6 +3,7 @@
 # ts-node /usr/src/app/node_modules/typeorm/cli.js migration:run --dir /usr/src/app/dataSource.ts
 
 pnpm run db:migrate
+# node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:run -d ./dataSource.js
 
 node /usr/src/app/dist/src/main.js
 
