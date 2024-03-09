a7876e3bdb2c8ac95081c4baa01ffa327b5c11c7
Sungyong Cho <dev@sungyongcho.com>
2024-03-09 02:50:56 +0100

    chore: startup changed to run the pnpm instead

--- back/startup.sh
+++ back/startup.sh
@@ -2,7 +2,7 @@
 
 # ts-node /usr/src/app/node_modules/typeorm/cli.js migration:run --dir /usr/src/app/dataSource.ts
 
-npm run db:migrate
+pnpm run db:migrate
 
 node /usr/src/app/dist/src/main.js
 
